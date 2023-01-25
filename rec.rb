8f04b06af70357fc36a73bd373a9113db8fa738f
“Lissa <lwarshaw674@gmail.com>
2023-01-25 14:10:18 -0500

    update

--- rec.rb
+++ rec.rb
@@ -1,17 +1,24 @@
             #1             #2
 
-def range(start_range, end_range)
-
-    return start_range if start_range == end_range
+def range(start, end_number)
+    #[1,5]
+    #[1,2,3,4,5]
     
-    new_arr = []
+    return [] if end_number < start
+    return start if start == end_number - 1
 
-    new_arr << range(start_range, end_range - 1)
+    new_arr = []
+    new_arr << start 
+    # add_arr = [range(start + 1, end_number)] 
+    #range[start, end_number - 1] << end_number
+    new_arr.concat(add_arr)
 
     return new_arr
 
+    
 end
 
-# range(1, 5)
-puts range(1, 4)
+
+p range(1, 5) 
+p range(1, 4) 
 # range(1, 3)
