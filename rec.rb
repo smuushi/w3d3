661f5ecaf7f2d7a626fa3dfa126ed928f61cedf6
“Lissa <lwarshaw674@gmail.com>
2023-01-25 15:38:53 -0500

    yay

--- rec.rb
+++ rec.rb
@@ -66,3 +66,34 @@ p range(1, 4)
   end
 
   p rec_2(3,4)
+ 
+
+  def deep_dup(arr)
+    return arr.dup if arr.all? { |ele| !ele.is_a?(Array) }  
+    
+    new_arr = []
+
+    arr.each do |ele|
+      if ele.is_a?(Array) 
+        new_arr << deep_dup(ele) 
+      
+      else
+        new_arr << ele.dup
+      end
+    end
+
+    return new_arr
+
+  end
+
+  robot_parts = [
+  ["nuts", "bolts", "washers"],
+  ["capacitors", "resistors", "inductors"]
+]
+  robot_parts_copy = deep_dup(robot_parts)
+
+  robot_parts_copy[1] << "LEDs"
+
+  p robot_parts[1]
+
+  p robot_parts_copy[1]
